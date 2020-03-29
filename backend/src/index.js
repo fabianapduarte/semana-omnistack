cb42ab1e8dbb017e0eb63f0a8aeae9e19d59c669
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2020-03-29 16:58:11 -0300

    adicionando validação nas rotas com o celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // converter o corpo da requisição em json
 app.use(routes); //deve estar abaixo do express
+app.use(errors());
 
 app.listen(3333);
